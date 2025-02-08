6e77004d162baeb68bb58fdfe9da5a1a30b30637
CocaIsMee <anhphukareno@gmail.com>
2025-02-08 21:43:59 +0700

    setup tailwind and base landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './component/Layout/Header';
-import Footer from './component/Layout/Footer';
-import Navbar from './component/Layout/Navbar';
-import Home from './component/pages/Home';
-import Profile from './component/pages/Profile';
-import Settings from './component/pages/Setting';
+import Header from './Layout/Header';
+import Footer from './Layout/Footer';
+import Navbar from './Layout/Navbar';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Settings from './pages/Setting';
 
 const App = () => {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
+      <div>
         <Header />
         <Navbar />
-        <main className="flex-grow">
+        <main >
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
