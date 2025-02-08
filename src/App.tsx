d0f2ea1b05e9373f7d72dd2b1fa1c9df36895ef2
CocaIsMee <anhphukareno@gmail.com>
2025-02-09 00:05:54 +0700

    update dashboard admin, navigate, routerrouter

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,20 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Login from "./pages/login";
 import Settings from "../src/pages/Setting";
+import Dashboard from "./pages/admin/dashboard";
+import Users from "./pages/admin/user";
+import Manage from "./pages/admin/manage";
+import DashboardLayout from "./component/dashboard";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<DashboardLayout />} />
+        <Route path="dbhome" element={<Dashboard />} />
+        <Route path="users" element={<Users />} />
+        <Route path="manage" element={<Manage />} />
         <Route path="/home" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="profile" element={<Profile />} />
